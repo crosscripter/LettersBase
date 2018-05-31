050e6c4029835bdd162fe1922ff0fc5153274b2d
crosscripter <crosscripter@gmail.com>
2018-05-31 14:09:05 -0500

    Initial commit

---
+++ consoleapplication1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Public Function ToLetters(ByVal Value As Integer) As String
+        Const Base As Integer = 26
+        Dim ModNum As Integer = 0
+        Dim Letters As String = String.Empty
+
+        While Value > 0
+            ModNum = (Value - 1) Mod Base
+            Letters = Chr(65 + ModNum) + Letters
+            Value = (Value - ModNum) / Base
+        End While
+
+        Return Letters
+    End Function
+
+    Sub Main()
+        Console.WriteLine(ToLetters(Integer.MaxValue))
+    End Sub
+End Module
